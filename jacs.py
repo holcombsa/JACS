be66ebd9954ca69e5890026978fb1ce8229c1f35
holcombsa <113376259+holcombsa@users.noreply.github.com>
2023-02-22 17:01:01 -0500

    Update jacs.py

--- jacs.py
+++ jacs.py
@@ -1,12 +1,18 @@
 #! /usr/bin/env python3
 
-from random import choice; import sys, getpass
+import sys
 
 # alfa = "Z.Cak" + chr(34) + "BY_4AuXb)5Wlv,V6-cUm7]Tw|" + chr(10) + "dS8(nRx':9/QeoP!yO" + chr(32) + "f@Np<zM#g+Lq{$K0;hJ%rI>1}^Hi" + chr(92) + "sG=&2Fj*Et[3D?"
 alfa = "qwertyuiopasdfghjklzxcvbnm" + chr(34) + "0123456789" + chr(92) + "~`!@#$%^&*()_+-=[]{}|':;<>?/.," + chr(10) + "QWERTYUIOPASDFGHJKLZXCVBNM" + chr(32)
 alfa_plus = alfa * 3
 
-  
+
+
+def char_err(char):
+  return color(f"Character not recogized: {char}", "red")
+
+
+
 def pw_error(pwe):
   incompatible_password = "Your password must contain a minimum of:\n1 uppercase letter\n1 lowercase letter\n1 number\n1 symbol\n12 characters\nno more than 2 identical characters in succession\nno more than 2 sequential characters"
   return input(color(f"{incompatible_password}\n>> {pwe} <<", "red") + "\nTry another password ")
@@ -16,6 +22,9 @@ def pw_error(pwe):
 def pw_check(pw):
   up, lo, nu, sy, char_cnt = 0,0,0,0,0
   for char in pw:
+    if char not in alfa:
+      print(char_err(char))
+      return pw_check(pw_error(pw))
     if char_cnt != 0:
       if char_cnt != len(pw)-1:
         previous_char, next_char, char_lo = pw[char_cnt-1].lower(), pw[char_cnt+1].lower(), char.lower()
@@ -42,9 +51,15 @@ def pw_check(pw):
 
   
 def gen_pkey(pw_true):
-  pkey_ls = [str(ord(i)) for i in pw_true]
-  pkey = "".join(pkey_ls).replace("0", "")
-  return pkey
+  print("... standby ...")
+  pkey_ls = [ord(i) for i in pw_true]
+  pkey_temp = 1
+  for n in pkey_ls:
+    pkey_temp += n * n
+  pkey = pkey_temp ** pkey_temp
+  if str(000) in str(pkey):
+    pkey = str(pkey).replace("000","")
+  return str(pkey)
 
 
 
@@ -52,6 +67,7 @@ def encrypt(msg, passwd):
   passkey, emsg, i = pw_check(passwd), "", 0
   for char in msg:
     num_key, two_digs = int(passkey[i]), int(passkey[i:i+2])
+    if char not in alfa: return char_err(char)
     if i >= len(passkey)-1: i = 0
     if num_key % 2 == 0:
       emsg += alfa_plus[alfa.index(char)+two_digs]
@@ -70,6 +86,7 @@ def decrypt(emsg, passwd):
   for char in emsg:
 #   for char in emsg[::3]:
     num_key, two_digs = int(passkey[i]), int(passkey[i:i+2])
+    if char not in alfa: return char_err(char)
     if i >= len(passkey)-1: i = 0
     if num_key % 2 != 0:
       dmsg += alfa_plus[alfa.index(char)+two_digs]
@@ -115,10 +132,10 @@ def typed():
   msg = input("Enter message: ")
   passwd = input("Enter password: ")
   crypt = input("Encrypt or decrypt? e/d ")
-  if crypt.lower() == "e":
+  if crypt.lower() == "e" or crypt.lower() == "encrypt":
     emsg = encrypt(msg.replace("\\n", chr(10)), passwd)
     return color(emsg.replace(chr(10), "\\n"), "green")
-  elif crypt.lower() == "d":
+  elif crypt.lower() == "d" or crypt.lower() == "decrypt":
     dmsg = decrypt(msg.replace("\\n", chr(10)), passwd)
     return color(dmsg, "blue")
   else: again = input("Something's wrong. Try again? y/n ")
@@ -127,7 +144,6 @@ def typed():
 
   
 try: file_access(sys.argv[1])
-except: pass
-# except: print(typed())
-# finally: pass
-
+# except: pass
+except: print(typed())
+finally: pass
